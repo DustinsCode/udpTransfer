9ae8c5c0ad9cbd32fed65e2f71d920b4e7f4b4cf
Dustin Thurston <dthurston1996@gmail.com>
2017-10-23 19:36:54 -0400

    ugh

--- Server/server.java
+++ Server/server.java
@@ -7,11 +7,11 @@ import java.nio.file.*;
 import java.util.*;
 
 /**
- * UDP File Transfer Project
- *
- * @author Dustin Thurston
- * @author Ryan Walt
- * */
+* UDP File Transfer Project
+*
+* @author Dustin Thurston
+* @author Ryan Walt
+* */
 
 class server{
     private static final int TIMEOUT = 3000;
@@ -57,86 +57,52 @@ class server{
 
             while(true){
 
-              //Buffer for the name of the file requested by the client.
-              ByteBuffer fileNameBuf = ByteBuffer.allocate(1024);
+                //Buffer for the name of the file requested by the client.
+                ByteBuffer fileNameBuf = ByteBuffer.allocate(1024);
 
-			  //testing
+                ds.setSoTimeout(3000);
 
-			  ds.setSoTimeout(3000);
+                DatagramPacket namePacket = new DatagramPacket(new byte[1024], 1024);
+                File clientFile;
 
-			  DatagramPacket namePacket = new DatagramPacket(new byte[1024], 1024);
-			  File clientFile;
+                while(true){
+                    try{
+                        ds.receive(namePacket);
+                        client = namePacket.getSocketAddress();
+                        String fileName = new String(namePacket.getData());
+                        fileName = fileName.trim();
+                        fileName = fileName.substring(1,fileName.length());
 
-			  while(true){
-				  try{
-					  ds.receive(namePacket);
-					  client = namePacket.getSocketAddress();
-					  String fileName = new String(namePacket.getData());
-		              fileName = fileName.trim();
-					  fileName = fileName.substring(1,fileName.length());
+                        //Search for the file in the directory of the server.
+                        clientFile = findFile(fileName);
 
-					  //Search for the file in the directory of the server.
-		              clientFile = findFile(fileName);
+                        //TODO: change this so that it sends a packet
+                        if (clientFile == null){
+                            System.out.println("File not found on the server.");
+                            ByteBuffer errorBuf = ByteBuffer.wrap("filenotfound".getBytes());
+                            c.send(errorBuf, client);
+                        }else{
+                            fileSize = (int)clientFile.length();
+                            //Get the total number of packets to send to the client.
 
-					  //TODO: change this so that it sends a packet
-					  if (clientFile == null){
-			                System.out.println("File not found on the server.");
-			                ByteBuffer errorBuf = ByteBuffer.wrap("filenotfound".getBytes());
-							c.send(errorBuf, client);
-					  }else{
-						  fileSize = (int)clientFile.length();
-		                  //Get the total number of packets to send to the client.
+                            if (fileSize % 1024 == 0)
+                            numPackets = fileSize / 1024;
+                            else
+                            numPackets = (fileSize / 1024) + 1;
 
-		                  if (fileSize % 1024 == 0)
-		                    numPackets = fileSize / 1024;
-		                  else
-		                    numPackets = (fileSize / 1024) + 1;
-
-		                  String tempPacketString = numPackets + "";
-		                  //ByteBuffer numPacketsBuf = ByteBuffer.wrap(tempPacketString.getBytes());
-						  DatagramPacket sizePacket = new DatagramPacket(tempPacketString.getBytes(), tempPacketString.getBytes().length, client);
-		                  ds.send(sizePacket);
-					  }
+                            String tempPacketString = numPackets + "";
+                            //ByteBuffer numPacketsBuf = ByteBuffer.wrap(tempPacketString.getBytes());
+                            DatagramPacket sizePacket = new DatagramPacket(tempPacketString.getBytes(), tempPacketString.getBytes().length, client);
+                            ds.send(sizePacket);
+                        }
 
 
-					  break;
-				  }catch(SocketTimeoutException ste){
-					  //System.out.println("Timed out on file request");
-				  }
-			  }
-			  //testing
+                        break;
+                    }catch(SocketTimeoutException ste){
+                        //System.out.println("Timed out on file request");
+                    }
+                }
 
-/****************
-              client = c.receive(fileNameBuf);
-              String fileName = new String(fileNameBuf.array());
-              fileName = fileName.trim();
-              System.out.println("Client wants: " + fileName);
-              fileName = fileName.substring(1,fileName.length());
-
-              //Search for the file in the directory of the server.
-              File clientFile = findFile(fileName);
-
-
-              if (clientFile == null){
-                System.out.println("File not found on the server.");
-                ByteBuffer errorBuf = ByteBuffer.wrap("filenotfound".getBytes());
-                c.send(errorBuf, client);
-              }
-
-              if(clientFile != null){
-                //Get the size of the file
-                fileSize = (int)clientFile.length();
-                //Get the total number of packets to send to the client.
-
-                if (fileSize % 1024 == 0)
-                  numPackets = fileSize / 1024;
-                else
-                  numPackets = (fileSize / 1024) + 1;
-
-                String tempPacketString = numPackets + "";
-                ByteBuffer numPacketsBuf = ByteBuffer.wrap(tempPacketString.getBytes());
-                c.send(numPacketsBuf, client);
-**************/
                 packetBoolArray = new boolean[numPackets];
                 Arrays.fill(packetBoolArray, false);
                 packetArray = new DatagramPacket[numPackets];
@@ -148,22 +114,29 @@ class server{
                 bytesToSend = 0;
                 numSent = 0;
                 boolean empty = true;
+                int loops = 0;
 
-                while (numSent < numPackets){
-                  if (empty)
-                    sendStandard();
-                  else{
-                    sendMissing();
-                  }
-                  ArrayList<Integer> ackArray = getAck();
-                  setNulls(ackArray, packetArray);
+                while (numSent < numPackets || !empty){
+                    if (empty){
+                        sendStandard();
+                        loops = 0;
+                    }else{
+                        sendMissing();
+                        loops++;
+                    }
+                    ArrayList<Integer> ackArray = getAck();
+                    setNulls(ackArray, packetArray);
 
-                  empty = isEmpty(packetArray);
 
+                    empty = isEmpty(packetArray);
+
+                    if(loops > 10){
+                        break;
+                    }
                 }
                 System.out.println("File sent!");
-              }
-           // }
+            }
+            // }
         }catch(IOException e){
             System.out.println("Got an io exception ya dingus");
 
@@ -171,133 +144,133 @@ class server{
     }
 
     public static void sendMissing() throws IOException{
-      for (int i = 0; i < packetArray.length; i++){
-        int indexToSend;
-        if (packetArray[i] != null){
-          indexToSend = i;
-          ds.send(packetArray[indexToSend]);
-          System.out.println("Lost packet sent: " + indexToSend);
+        for (int i = 0; i < packetArray.length; i++){
+            int indexToSend;
+            if (packetArray[i] != null){
+                indexToSend = i;
+                ds.send(packetArray[indexToSend]);
+                System.out.println("Lost packet sent: " + indexToSend);
+            }
         }
-      }
-      //ds.send(packetArray[indexToSend]);
+        //ds.send(packetArray[indexToSend]);
     }
 
     public static void sendStandard(){
-      int count = 0;
+        int count = 0;
 
-      try{
-        while (count < 5){
-          if (fileSize - bytesSent < 1024)
-          bytesToSend = fileSize - bytesSent;
-          else
-          bytesToSend = 1024;
+        try{
+            while (count < 5){
+                if (fileSize - bytesSent < 1024)
+                bytesToSend = fileSize - bytesSent;
+                else
+                bytesToSend = 1024;
 
-          if(bytesToSend < 0){
-            break;
-          }
-          int tempNumSent = numSent;
-          byte[] sendBytes = new byte[bytesToSend + 3];
-          byte b3 = (byte)(tempNumSent & 0xFF);
-          byte b2 = (byte)((tempNumSent >> 8) & 0xFF);
-          byte b1 = (byte)((tempNumSent >> 16)&0xFF);
-          sendBytes[0] = b1;
-          sendBytes[1] = b2;
-          sendBytes[2] = b3;
+                if(bytesToSend < 0){
+                    break;
+                }
+                int tempNumSent = numSent;
+                byte[] sendBytes = new byte[bytesToSend + 3];
+                byte b3 = (byte)(tempNumSent & 0xFF);
+                byte b2 = (byte)((tempNumSent >> 8) & 0xFF);
+                byte b1 = (byte)((tempNumSent >> 16)&0xFF);
+                sendBytes[0] = b1;
+                sendBytes[1] = b2;
+                sendBytes[2] = b3;
 
-          bis.read(sendBytes, 3, bytesToSend);
+                bis.read(sendBytes, 3, bytesToSend);
 
-          DatagramPacket d = new DatagramPacket(sendBytes, bytesToSend+3, client);
-          Random r = new Random();
-          int ranNum = r.nextInt(100);
+                DatagramPacket d = new DatagramPacket(sendBytes, bytesToSend+3, client);
+                Random r = new Random();
+                int ranNum = r.nextInt(100);
 
-          ds.send(d);
+                ds.send(d);
 
-          // if (ranNum < 10){
-          //   ds.send(d);
-          //   System.out.println("Packet sent: " + tempNumSent);
-          // }
+                // if (ranNum < 10){
+                //   ds.send(d);
+                //   System.out.println("Packet sent: " + tempNumSent);
+                // }
 
-          System.out.println("Packet sent: " + tempNumSent);
-          packetArray[numSent] = d;
+                System.out.println("Packet sent: " + tempNumSent);
+                packetArray[numSent] = d;
 
-          bytesSent += 1024;
-          numSent++;
-          count++;
+                bytesSent += 1024;
+                numSent++;
+                count++;
+            }
         }
-      }
-      catch(IOException e){
-        System.out.println("IOException!");
-      }
+        catch(IOException e){
+            System.out.println("IOException!");
+        }
 
     }
 
 
     public static ArrayList<Integer> getAck() throws IOException{
-      int count = 0;
+        int count = 0;
 
-      ArrayList<Integer> ackArray = new ArrayList<>();
-      while (count < 5){
-        DatagramPacket tempPacket = new DatagramPacket(new byte[1024], 1024);
-        try{
-          ds.setSoTimeout(TIMEOUT);
-          ds.receive(tempPacket);
-          //System.out.println("received ack");
+        ArrayList<Integer> ackArray = new ArrayList<>();
+        while (count < 5){
+            DatagramPacket tempPacket = new DatagramPacket(new byte[1024], 1024);
+            try{
+                ds.setSoTimeout(TIMEOUT);
+                ds.receive(tempPacket);
+                //System.out.println("received ack");
 
-          String tempString = new String(tempPacket.getData());
-          tempString = tempString.trim();
-          int tempNum = Integer.parseInt(tempString);
-          System.out.println("received ack: " + tempNum);
+                String tempString = new String(tempPacket.getData());
+                tempString = tempString.trim();
+                int tempNum = Integer.parseInt(tempString);
+                System.out.println("received ack: " + tempNum);
 
-          ackArray.add(tempNum);
+                ackArray.add(tempNum);
 
+            }
+            catch(SocketTimeoutException e){
+                System.out.println("timed out");
+                break;
+            }
+            if(numPackets == ackArray.size())
+            break;
+            count++;
         }
-        catch(SocketTimeoutException e){
-          System.out.println("timed out");
-          break;
-        }
-        if(numPackets == ackArray.size())
-          break;
-        count++;
-      }
-      return ackArray;
+        return ackArray;
     }
 
     public static void setNulls(ArrayList<Integer> arr, DatagramPacket[] packets){
-      for (int i : arr){
-        packets[i] = null;
-      }
+        for (int i : arr){
+            packets[i] = null;
+        }
     }
 
     public static boolean isEmpty(DatagramPacket[] packets){
-      for (int i = 0; i < packets.length; i++){
-        if (packets[i] != null)
-          return false;
-      }
-      return true;
+        for (int i = 0; i < packets.length; i++){
+            if (packets[i] != null)
+            return false;
+        }
+        return true;
     }
 
 
     //Return the file the user searched for.
     public static File findFile(String type){
-      File sendFile = null;
-      File dir = new File(server.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-      newType = type.trim();
+        File sendFile = null;
+        File dir = new File(server.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        newType = type.trim();
 
-      File[] matches = dir.listFiles(new FilenameFilter() {
-        public boolean accept(File dir, String name) {
-          return name.startsWith(newType);
-        }
-      });
+        File[] matches = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.startsWith(newType);
+            }
+        });
 
-      if (matches.length < 1)
-      return null;
-      else{
-        for(int i = 0; i < matches.length; i++){
-          //System.out.println(matches[i]);
-          sendFile = matches[0];
+        if (matches.length < 1)
+        return null;
+        else{
+            for(int i = 0; i < matches.length; i++){
+                //System.out.println(matches[i]);
+                sendFile = matches[0];
+            }
+            return sendFile;
         }
-        return sendFile;
-      }
 
     }
-  }
+}
